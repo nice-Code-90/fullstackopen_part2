9292ed15d5cbe367e746b03bf52255849746f46f
Milan Mazik <mazik1990@hotmail.com>
2024-05-17 13:05:29 +0200

    2.1-2.5

--- courseinfo/src/components/Course.jsx
+++ courseinfo/src/components/Course.jsx
@@ -1,15 +1,16 @@
 import Header from "./Header";
 import Content from "./Content";
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
-
-
-
 const Course = ({ course }) => {
+  const total = course.parts.reduce(
+    (points, parts) => points + parts.exercises,
+    0
+  );
   return (
     <>
       <Header title={course.name} />
       <Content parts={course.parts} />
+      <h3>total of {total} exercises</h3>
     </>
   );
 };
