308f22c0b29901aeaa4ef13e7fd8670fcca82fe5
Milan Mazik <mazik1990@hotmail.com>
2024-05-22 16:06:24 +0200

    todo: languages

---
+++ countries/src/App.jsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [fetchedCountries, setFetchedCountries] = useState([]); //fetched all countries
+
+  const [countryFilter, setCountryFilter] = useState(""); //field value of form
+  const [filteredResults, setFilteredResults] = useState([]); //filtered results
+
+  const [searchField, setSearchField] = useState(null); // value of submitted field
+
+  useEffect(() => {
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then((response) => {
+        setFetchedCountries(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching countries:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    setFilteredResults(
+      fetchedCountries.filter((country) =>
+        country.name.common.toLowerCase().includes(searchField.toLowerCase())
+      )
+    );
+  }, [searchField]);
+
+  useEffect(() => {
+    console.log(filteredResults);
+  }, [filteredResults]);
+
+  const onSearch = (e) => {
+    e.preventDefault();
+    setSearchField(countryFilter);
+  };
+
+  return (
+    <>
+      <form onSubmit={onSearch}>
+        find countries
+        <input
+          value={countryFilter}
+          onChange={(e) => {
+            setCountryFilter(e.target.value);
+          }}
+        ></input>
+      </form>
+      <div>
+        {filteredResults.length > 10 ? (
+          <p>Too many matches, specify another filter</p>
+        ) : filteredResults.length > 1 && filteredResults.length < 11 ? (
+          filteredResults.map((country) => (
+            <p key={country.name.common}>{country.name.common}</p>
+          ))
+        ) : filteredResults.length === 1 ? (
+          <>
+            <h1>{filteredResults[0].name.common}</h1>
+
+            <p>capital {filteredResults[0].capital[0]}</p>
+            <p>area {filteredResults[0].area}</p>
+            <div>
+              <h2>languages</h2>
+              {filteredResults[0].languages.values.map((lang) => (
+                <p>{lang}</p>
+              ))}
+            </div>
+          </>
+        ) : (
+          <p>No matches found</p>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
